57f2e782e486cbaa76cb7110081ec19100d9b023
Michael Aufreiter <michael@substance.io>
2017-02-09 21:26:07 +0100

    Comments setup.

--- make.js
+++ make.js
@@ -38,6 +38,7 @@ b.task('clean', function() {
 
 b.task('example', function() {
   b.copy('./example/index.html', './dist/')
+  b.copy('./example/comments.html', './dist/')
 })
 
 b.task('lib', function() {
